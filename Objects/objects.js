f0bca8ded5556c08eaa21fc3071ca419664a407b
Neisy Wilson <neisywilson@gmail.com>
2016-11-05 08:31:24 -0400

    Objects Basic Example

---
+++ Objects/objects.js
@@ -0,0 +1,27 @@
+// Objects in JavaScript group related data and functions together.
+
+// Basic Example:
+
+var myProperty = {
+  address: "32 Main",
+  city: "Toronto",
+  purchaseYear: 2016
+};
+
+myProperty
+
+// Console results:
+// Object {address:"32 Main", city: "Toronto", purchaseYear: 2016}
+
+// Accesing an specific part(data) of the Object:
+myProperty.city
+//  Console results:
+// "Toronto"
+
+myProperty.address
+//  Console results:
+// "32 Main"
+
+myProperty.purchaseYear
+//  Console results:
+// "2016"
